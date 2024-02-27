330f693c272e056c30e6144b9512f5a2277120ba
aritra <aritrarick2002@gmail.com>
2024-02-28 00:57:15 +0530

    Added req dependencies ♻️

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,12 +54,18 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.wear.compose:compose-material:1.1.2")
-    implementation("androidx.wear.compose:compose-foundation:1.1.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.wear.compose:compose-material:1.3.0")
+    implementation("androidx.wear.compose:compose-foundation:1.3.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.core:core-splashscreen:1.0.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Material
+    implementation("androidx.compose.material:material-icons-extended:1.6.2")
+
+    // Navigation
+    implementation ("androidx.wear.compose:compose-navigation:1.3.0")
 }
